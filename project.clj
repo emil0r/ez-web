881b253a8a98d84ea3fd60a1ffc46741bf24eb2b
Emil Bengtsson <emil@emil0r.com>
2014-06-14 00:02:10 +0200

    0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ez-web "0.1.1-SNAPSHOT"
+(defproject ez-web "0.1.1"
   :description "ez-web, batteries for the web"
   :url "https://github.com/emil0r/ez-web"
   :license {:name "Eclipse Public License"
